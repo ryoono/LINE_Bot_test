9963cf1048bb2d08a393e9465699968944f282de
k16025kk <ono@pluslab.org>
2021-05-16 01:46:38 +0900

    test

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def callback():
 
     # get request body as text
     #body = request.get_data(as_text=True)
-    body = "固定メッセージ(test)"
+    body = '固定メッセージ(test)'
     app.logger.info("Request body: " + body)
 
     # handle webhook body
