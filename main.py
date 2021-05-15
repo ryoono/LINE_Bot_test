65afbc38aeadf07a4e2514335ed2524cf770f4b8
k16025kk <ono@pluslab.org>
2021-05-16 02:04:54 +0900

    add file(create return message)

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
 import os
 import sys
 from argparse import ArgumentParser
@@ -42,7 +30,7 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-
+# Webhookからのリクエストをチェック
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -60,7 +48,9 @@ def callback():
 
     return 'OK'
 
-
+# LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に実行
+# reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークン
+# 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクト
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     line_bot_api.reply_message(
